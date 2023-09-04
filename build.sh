30c4388c8849a39954f2bb47493eb1defbd326d4
Jack Le Hamster <jacklehamster@gmail.com>
2023-09-04 01:43:20 -0700

    Adding sound

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 echo "================================="
 rm game.zip
-uglifyjs --mangle toplevel  decode.js g.js -o release/gg.js
+uglifyjs --mangle toplevel  decode.js g.js sound/soundbox.js -o release/gg.js
 cp play.html release
 cp rider.13k release
+cp sound/fur-elise.sbox
+cp sound/wild-horde.sbox
 zip -vr game.zip release/ -x "*.DS_Store"
 echo "=================================="
 ls -lh game.zip
